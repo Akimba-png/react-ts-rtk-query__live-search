035ca6190011b66b556e6d03aea39db54cd4ca17
Oleg <akimba@gmail.com>
2022-12-03 13:22:46 +0300

    feat: Добавляет отрисовку избранных репозиториев

--- src/pages/favorites-page.tsx
+++ src/pages/favorites-page.tsx
@@ -1,6 +1,20 @@
+import { RepoItem } from '../components/repo-item';
+import { useAppSelector } from './../store/store';
+
 function FavoritesPage(): JSX.Element {
+  const favorites = useAppSelector((state) => state['favorite-reducer'].favorites);
   return (
-    <h1>Favorites Page</h1>
+    <main className='pt-[50px] h-[calc(100vh_-_79px)]'>
+      <h1 className='mb-[40px] text-center font-bold text-2xl'>Избранные репозитории</h1>
+      <section className='w-[540px] mx-auto'>
+        {!favorites.length && <p>Нет избранных репозиториев</p>}
+        <ul>
+          {
+            favorites.map((item) => <RepoItem repo={item}/>)
+          }
+        </ul>
+      </section>
+    </main>
   );
 }
 
